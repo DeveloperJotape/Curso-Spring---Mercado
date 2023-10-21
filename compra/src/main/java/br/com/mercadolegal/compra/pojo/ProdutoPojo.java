f53e7aeb5c4b52a32af7cc056e586a4634bc9c74
jp <developerjotape@gmail.com>
2023-10-21 13:23:01 -0300

    Criado: Classe ProdutoPojo

--- compra/src/main/java/br/com/mercadolegal/compra/pojo/ProdutoPojo.java
+++ compra/src/main/java/br/com/mercadolegal/compra/pojo/ProdutoPojo.java
@@ -1,5 +1,65 @@
 package br.com.mercadolegal.compra.pojo;
 
-public class ProdutoPojo {
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.mercadolegal.compra.entidade.Produto;
+
+public class ProdutoPojo {
+	
+	private Long idProduto;
+	private String nomeProduto;
+	private String categoriaProduto;
+	private Double valorProduto;
+	
+	public ProdutoPojo() {}
+	
+	public ProdutoPojo(Produto produto) {
+		this.idProduto = produto.getIdProduto();
+		this.nomeProduto = produto.getNomeProduto();
+		this.categoriaProduto = produto.getCategoriaProduto();
+		this.valorProduto = produto.getValorProduto();
+	}
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getCategoriaProduto() {
+		return categoriaProduto;
+	}
+
+	public void setCategoriaProduto(String categoriaProduto) {
+		this.categoriaProduto = categoriaProduto;
+	}
+
+	public Double getValorProduto() {
+		return valorProduto;
+	}
+
+	public void setValorProduto(Double valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+	
+	@JsonIgnore
+	public Produto toEntity() {
+		Produto produto = new Produto();
+		produto.setIdProduto(idProduto);
+		produto.setNomeProduto(nomeProduto);
+		produto.setCategoriaProduto(categoriaProduto);
+		produto.setValorProduto(valorProduto);
+		return produto;
+	}
+	
 }
